8eecdbf740ac479baf5e3d376f770449f5348533
Diego Gomez <dgomez@dgomez-ml1.local>
2024-04-07 22:21:07 -0400

    Hide sports tab for a while

--- app/(main)/(main)/layout.jsx
+++ app/(main)/(main)/layout.jsx
@@ -17,7 +17,7 @@ export default function Layout({ children }) {
     { name: "Inicio", to: "/", icon: "home", permissionRequired: "None" },
     { name: "Reserva multicancha", to: "/calendar", icon: "calendar_month", permissionRequired: "None" },
     { name: "Reserva gimnasio", to: "/gym_calendar", icon: "calendar_month", permissionRequired: "None" },
-    { name: "Deportes", to: "/sports", icon: "sports", permissionRequired: "None" },
+    // { name: "Deportes", to: "/sports", icon: "sports", permissionRequired: "None" },
     // { name: "Perfil", to: "/profile", icon: "face", permissionRequired: "User" },
     // { name: "Users", to: "/users", icon: "group", permissionRequired: "Staff" },
     { name: "Contacto", to: "/contact", icon: "support_agent", permissionRequired: "None" },
